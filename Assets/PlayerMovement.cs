ebe2eb544e24be26b3dcd5f55e22f5fa094fd264
Anghelo312 <angelo.conteron@en.edu.ec>
2026-03-03 12:46:27 -0500

    init: proyecto CartoonFighter

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    public float jumpForce = 10f;
+    private Rigidbody2D rb;
+    private bool isGrounded;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        // Movimiento horizontal
+        float moveX = Input.GetAxis("Horizontal");
+        rb.linearVelocity = new Vector2(moveX * speed, rb.linearVelocity.y);
+
+        // Salto
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        isGrounded = true;
+    }
+
+    void OnCollisionExit2D(Collision2D col)
+    {
+        isGrounded = false;
+    }
+}

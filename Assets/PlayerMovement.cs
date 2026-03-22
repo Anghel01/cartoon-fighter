2342e1754bc6d212e52f00f60c0b4d8abbc97e9b
Angelo Israel Conterón Santillán <angelo.conteron@epn.edu.ec>
2026-03-22 13:18:44 -0500

    feat: player animations with Animator Controller

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+
 public class playerMovement : MonoBehaviour
 {
     public float speed = 5f; // establece la velocidad de movimiento del jugador a 5 unidades por segundo
@@ -6,12 +7,16 @@ public class playerMovement : MonoBehaviour
     private Rigidbody2D rb; // referencia al componente Rigidbody2D del jugador, rigibody2d es un componente que permite aplicar física a un objeto en 2D, 
     // es private porque solo se usará dentro de esta clase
     private bool isGrounded; // variable booleana para verificar si el jugador está en el suelo, es private porque solo se usará dentro de esta clase, isGrounded es una variable que se utiliza para determinar si el jugador está tocando el suelo, lo que es necesario para permitir que el jugador salte solo cuando está en el suelo
+    private Animator anim;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>(); // obtiene el componente Rigidbody2D del jugador y lo asigna a la variable rb
+        anim = GetComponent<Animator>();
+
     }
     void Update()
     {
+
         float moveX = Input.GetAxis("Horizontal"); // obtiene el valor del eje horizontal (teclas A/D o flechas izquierda/derecha) y lo asigna a la variable moveX
 
         rb.linearVelocity = new Vector2(moveX * speed, rb.linearVelocity.y); // establece la velocidad horizontal del jugador multiplicando el valor de moveX por la velocidad y manteniendo la velocidad vertical actual
@@ -29,6 +34,8 @@ public class playerMovement : MonoBehaviour
         {
             transform.localScale = new Vector3(-5, 5, 1);
         }
+        anim.SetFloat("Speed", Mathf.Abs(moveX));
+        anim.SetBool("IsGrounded", isGrounded);
     }
     void OnCollisionEnter2D(Collision2D col)
     {
